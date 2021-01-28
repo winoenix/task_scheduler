a4e07e52571b8b38cbbae56614b10a88bb636035
dingyi <winoenix@gmail.com>
2021-01-28 17:04:23 +0800

    增加block_t1，block次日这个时间

---
+++ task_sample/block_t1.py
@@ -0,0 +1,10 @@
+import sys
+from pandas import to_datetime
+import datetime
+import time
+
+if __name__ == "__main__":
+    block_time = to_datetime(sys.argv[1]) + datetime.timedelta(days=1)
+    while to_datetime(datetime.datetime.now()) < block_time:
+        time.sleep(1)
+    print("%s: block released. " % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
